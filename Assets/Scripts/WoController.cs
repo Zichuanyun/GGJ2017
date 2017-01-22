1e0ae5c3b6a1ea94dea671d69fd1ff6ebda4de81
FigoZhao <zhaoyifei19950308@hotmail.com>
2017-01-22 12:40:10 +0800

    wowowowowow

---
+++ Assets/Scripts/WoController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WoController: MonoBehaviour {
+    public AudioSource woSound;
+    public bool isPlaying = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            if (!woSound.isPlaying)
+                woSound.Play(0);
+        }
+        else
+        {
+            if(woSound.isPlaying)
+                woSound.Stop();
+        }
+    }
+    public void play()
+    {
+        if(!isPlaying)
+        isPlaying = true;
+    }
+    public void stop()
+    {
+        isPlaying = false;
+    }
+}
